40519616932d42f0f5e0e20bfc373d6532f1dcd8
Lukas Bystricky <lukasby@kth.se>
2020-04-15 15:54:35 +0200

    setup script for a periodic domain containing circles

---
+++ src/setups/circles_periodic.m
@@ -0,0 +1,45 @@
+function problem = circles_periodic(input_params)
+% Setup file for a doubly periodic array of circles       
+
+problem.Lx = input_params.box_size(1);
+problem.Ly = input_params.box_size(2);
+
+problem.plot_domain = input_params.plot_domain;
+problem.periodic = 1;
+problem.pressure_gradient_x = input_params.pressure_drop_x / problem.Lx;
+problem.pressure_gradient_y = input_params.pressure_drop_y / problem.Ly;
+problem.name = input_params.name;
+problem.gmres_tol = input_params.gmres_tol;
+problem.eta = input_params.eta;
+
+% discretize domain
+radii = input_params.radii;
+centers = input_params.centers;
+
+walls = cell(length(radii));
+
+for i = 1:length(radii)
+   walls{i} =  @(T) geometry_circle(radii(i),centers(i),T);
+end
+
+if length(input_params.panels) > 1
+    problem.panels = input_params.panels;
+else
+    problem.panels = input_params.panels*ones(length(walls),1);
+end
+
+problem.domain = discretize_domain_periodic(walls, problem.panels,...
+            problem.Lx, problem.Ly);
+
+% no-slip boundary conditions
+problem.boundary_conditions = @(z) 0*imag(z);
+
+
+if problem.plot_domain
+    plot_domain(problem);
+    disp('Simulation paused. Press any key to continue...');
+    pause;
+    disp('Continuing...');
+end
+
+
