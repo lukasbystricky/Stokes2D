a4cc1318ba08b4ad097b3bbd6943f144e859a659
Lukas Bystricky <lukasby@kth.se>
2020-04-15 10:04:49 +0200

    corrected periodic box

--- src/matlab/tools/plot_domain.m
+++ src/matlab/tools/plot_domain.m
@@ -3,11 +3,14 @@ function [] = plot_domain(problem)
 figure()
 hold on
 
-z = problem.domain.z;
-zp = problem.domain.zp;
-
 Lx = problem.Lx;
 Ly = problem.Ly;
+
+x = mod(real(problem.domain.z)+Lx/2,Lx)-Lx/2;
+y = mod(imag(problem.domain.z)+Ly/2,Ly)-Ly/2;
+
+z = x + 1i*y;
+zp = problem.domain.zp;
 
 plot(z, '-o');
 
@@ -17,6 +20,6 @@ quiver(real(z), imag(z), real(-1i*zp)./abs(zp), imag(-1i*zp)./abs(zp));
 xlim([min(real(z)), max(real(z))]);
 ylim([min(imag(z)), max(imag(z))]);
 
-rectangle('Position',[-Lx/2, -Lx/2, Lx, Ly], 'LineStyle', '--')
+rectangle('Position',[-Lx/2, -Ly/2, Lx, Ly], 'LineStyle', '--')
 
 axis equal
