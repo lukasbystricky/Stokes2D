b08fae6f488e9bb52ada1aa6b91a6e8f23c95d72
Lukas Bystricky <lukasby@kth.se>
2020-04-16 11:26:10 +0200

    minor edits

--- examples/demos/demo_periodic_curved_channel.m
+++ examples/demos/demo_periodic_curved_channel.m
@@ -11,7 +11,7 @@ input_params = default_input_params('curved_channel_demo', 1);
 
 % modify structure as needed, or add additional problem-dependent params
 input_params.box_size = [2,1.5];
-input_params.panels = 20;
+input_params.panels = 10;
 input.pressure_drop_x = 1;
 
 % set this up as a test problem. Shear flow boundary conditions are applied
