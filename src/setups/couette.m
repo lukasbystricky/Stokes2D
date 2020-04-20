aae30e731adf3d9e4bfb44ed70d71c03e8673a0b
Lukas Bystricky <lukasby@kth.se>
2020-04-20 12:21:12 +0200

    setup function for flow between rotating cylinders

---
+++ src/setups/couette.m
@@ -0,0 +1,48 @@
+function problem = couette(input_params)
+% Setup file a Couette apparatus, the outer wall is spinning, while all 
+% other walls are fixed       
+
+problem.plot_domain = input_params.plot_domain;
+problem.periodic = 0;
+problem.name = input_params.name;
+problem.gmres_tol = input_params.gmres_tol;
+
+% discretize domain
+radii = input_params.radii;
+centers = input_params.centers;
+omega = input_params.omega;
+
+walls = cell(length(radii));
+
+% the first wall is always the bounding wall and has a normal pointing in
+% the other direction to the other walls
+walls{1} =  @(T) geometry_circle(radii(1),centers(1),T, -1);
+
+if length(radii) > 1
+    for i = 2:length(radii)
+        walls{i} =  @(T) geometry_circle(radii(i),centers(i),T);
+    end
+end
+
+if length(input_params.panels) > 1
+    problem.panels = input_params.panels;
+else
+    problem.panels = input_params.panels*ones(length(walls),1);
+end
+
+problem.domain = discretize_domain(walls, problem.panels, centers);
+
+% zero on all walls except the outer, which is given by omega*[-y, x]
+problem.boundary_conditions = ...
+  @(z) omega*(abs(abs(z)-radii(1))<1e-12).*(-imag(z) + 1i*real(z));
+% problem.boundary_conditions = ...
+%   @(z) (-imag(z) + 1i*real(z))./(abs(z).^2);
+
+if problem.plot_domain
+    plot_domain(problem);
+    disp('Simulation paused. Press any key to continue...');
+    pause;
+    disp('Continuing...');
+end
+
+
