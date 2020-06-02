ce3ce92c5bcbe4c88342b161b1e6bb4058689412
Lukas Bystricky <lukasby@kth.se>
2020-06-02 16:08:35 +0200

    added support for mobility problem

--- src/matlab/solve_stokes.m
+++ src/matlab/solve_stokes.m
@@ -17,11 +17,19 @@ if problem.periodic
                 problem.gmres_tol, length(rhs));
 else
     % add rows for net force and torque on inner walls
-    nwalls = size(problem.domain.wall_indices, 1);
-    rhs = [rhs; zeros(3*(nwalls-1), 1)];
+    nwalls = size(problem.domain.wall_indices, 1);    
     
-    X = gmres(@(x) matvec_double_layer(x, problem.domain), rhs, [], ...
+    if problem.resistance 
+        rhs = [rhs; zeros(3*(nwalls-1), 1)];
+        
+        X = gmres(@(x) matvec_double_layer_resistance(x, problem.domain), rhs, [], ...
                 problem.gmres_tol, length(rhs));
+    else
+        rhs = [rhs; zeros(3*(nwalls), 1)];
+        
+        X = gmres(@(x) matvec_double_layer_mobility(x, problem.domain), rhs, [], ...
+                problem.gmres_tol, length(rhs));
+    end
 end
                         
 solution.q = [X(1:nsrc), X(nsrc+1:2*nsrc)];
@@ -31,10 +39,19 @@ if problem.periodic
     solution.u_avg = [X(end-1);X(end)];
 else
    % extract net force and torques
-   n_inner_walls = length(problem.domain.centers) - 1;
-   solution.forces = X(2*nsrc+1:2*nsrc+n_inner_walls) + ...
-            1i*X(2*nsrc+n_inner_walls+1:2*nsrc+2*n_inner_walls);
-   solution.torques = X(2*nsrc+2*n_inner_walls+1:end);     
+   if problem.resistance
+       n_inner_walls = length(problem.domain.centers) - 1;
+       
+       solution.forces = X(2*nsrc+1:2*nsrc+n_inner_walls) + ...
+                1i*X(2*nsrc+n_inner_walls+1:2*nsrc+2*n_inner_walls);
+       solution.torques = X(2*nsrc+2*n_inner_walls+1:end);   
+   else % extract translational and rotational velocities
+       n_particles = length(problem.domain.centers);
+       
+       solution.u_trans = X(2*nsrc+1:2*nsrc+n_particles) + ...
+                1i*X(2*nsrc+n_particles+1:2*nsrc+2*n_particles);
+       solution.omega = X(2*nsrc+2*n_particles+1:end);   
+   end
 end
     
 
