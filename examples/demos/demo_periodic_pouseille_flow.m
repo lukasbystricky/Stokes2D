56e8fcea51883c08fde667be61b9c2bddd942e87
Lukas Bystricky <lukasby@kth.se>
2020-09-21 16:32:55 +0200

    added check on pressure and velocity gradient

--- examples/demos/demo_periodic_pouseille_flow.m
+++ examples/demos/demo_periodic_pouseille_flow.m
@@ -14,8 +14,8 @@ clc
 input_params = default_input_params('pouseuille_demo', 1);
 
 % modify structure as needed
-input_params.box_size = [5,1.5];
-input_params.panels = 16;
+input_params.box_size = [5,3];
+input_params.panels = 5;
 input_params.eta = 1;
 
 problem = flat_pipe_periodic(input_params);
@@ -26,31 +26,37 @@ solution = solve_stokes(problem);
 % display solution
 exact_solution_u = @(x,y) -problem.pressure_gradient_x * y.*(y-1)/2;
 exact_solution_uy = @(x,y) -problem.pressure_gradient_x * (y - 0.5);
+exact_solution_p = @(x,y) problem.pressure_gradient_x * x;
 
-x = linspace(0,5);
-y = linspace(0.25, 0.75);
-[X,Y] = meshgrid(x,y);
-
-Uc = evaluate_velocity(solution, X, Y);
-%Pc = evaluate_pressure(solution, X, Y);
-
-[Ux, Uy, Vx, Vy, X, Y] = evaluate_velocity_gradient(solution, X, Y);
+x = linspace(0,5,20);
+y = linspace(0,1,20);
 
 
-subplot(2,1,1)
+[Uc, ~, X, Y] = evaluate_velocity(solution, 100);
+[Pc, P, ~, ~] = evaluate_pressure(solution, X, Y);
+[Uxc, Uyc, Vxc, Vyc, Ux, Uy, Vx, Vy] = evaluate_velocity_gradient(solution, X, Y);
+
+subplot(3,1,1)
+
 contourf(X,Y, log10(abs((Uc - exact_solution_u(X,Y))./...
     max(max(abs(exact_solution_u(X,Y)))))+eps));
 colorbar
+caxis([-16,-1]);
 axis equal
 title('velocity: log_{10}(relative error)');
 
-subplot(2,1,2)
-contourf(X,Y,log10(abs((Uy - exact_solution_uy(X,Y))./...
-    max(max(abs(exact_solution_uy(X,Y)))))+eps))
-%contourf(X,Y,log10(abs(Pc)+eps))
+subplot(3,1,2)
+contourf(X,Y,log10(abs(Pc - exact_solution_p(X,Y))+eps));
 colorbar
+caxis([-16,-1]);
 axis equal
-title('Uy: log_{10}(relative error)');
+title('P: log_{10}(relative error)');
 
+subplot(3,1,3)
+contourf(X,Y,log10(abs(Uyc - exact_solution_uy(X,Y))+eps));
+colorbar
+caxis([-16,-1]);
+axis equal
+title('u_y: log_{10}(relative error)');
 
 
