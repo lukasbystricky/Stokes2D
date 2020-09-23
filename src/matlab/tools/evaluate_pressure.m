03a6c7f8ff2e8ebec7dd0316bedde4fe3316c7bd
Lukas Bystricky <lukasby@kth.se>
2020-09-23 17:01:09 +0200

    added double-layer potential

--- src/matlab/tools/evaluate_pressure.m
+++ src/matlab/tools/evaluate_pressure.m
@@ -1,7 +1,8 @@
-function [Pc, X, Y, P] = evaluate_pressure(solution, varargin)
-%EVALUTATE_VELOCITY evaluates the pressure at either regular grid over the 
-%reference cell or at specified target points. Uses the stresslet identity 
-%to identify points inside the domain.
+function [Pc, P, X, Y] = evaluate_pressure(solution, varargin)
+%EVALUTATE_PRESSURE evaluates the pressure at either regular grid over the
+%reference cell or at specified target points. Uses the stresslet identity
+%to identify points inside the domain. Supports periodic and non-periodic 
+%domains, and uses special quadrature to evaluate close to the boundary.
 %
 % inputs:
 % -solution: structure containing the following fields
@@ -27,64 +28,115 @@ weights = solution.problem.domain.wazp;
 
 if nargin == 2 % N specified, evaluate on regular grid
     N = varargin{1};
-    x = linspace(min(xsrc), max(xsrc), N);
-    y = linspace(min(ysrc), max(ysrc), N);
+    if solution.problem.periodic
+        x = linspace(-Lx/2, Lx/2, N);
+        y = linspace(-Ly/2, Ly/2, N);
+    else
+        x = linspace(min(xsrc), max(xsrc), N);
+        y = linspace(min(ysrc), max(ysrc), N);
+    end
     
     [X,Y] = meshgrid(x,y);
-else % target points are specified
+else
     X = varargin{1};
-    Y = varargin{2};
+    Y = varargin{2};    
 end
 
-pslp = StokesSLP_pressure_ewald_2p(xsrc, ysrc, X(:), Y(:),...
-    solution.q(:,1).*weights, solution.q(:,2).*weights, Lx, Ly,...
-    'verbose', 1);
-
-% for special quadrature points must be inside reference cell
-Xtar_sq = mod(X+Lx/2,Lx)-Lx/2;
-Ytar_sq = mod(Y+Ly/2,Ly)-Ly/2;
-
-%correct using special quadrature
-[pslp_corrected,~] = mex_SQ_slp_pressure(Xtar_sq(:)+1i*Ytar_sq(:), domain.z, domain.zp,...
-    domain.quad_weights, domain.panel_breaks, domain.wazp, domain.z32,...
-    domain.zp32, domain.quad_weights32, domain.wazp32, ...
-    solution.q(:,1)+1i*solution.q(:,2),...
-    pslp,domain.mean_panel_length,domain.extra.gridSolidmat, ...
-    domain.extra.Nrows,domain.extra.Ncols,domain.extra.panels2wall,...
-    domain.reference_cell,true);
-
-if isinf(solution.problem.eta)
-    p = pslp;
-    p_corrected = pslp_corrected;
-else
-    
-    pdlp = StokesDLP_pressure_ewald_2p(xsrc, ysrc, X(:), Y(:), n1, n2,...
+if solution.problem.periodic
+    pslp = StokesSLP_pressure_ewald_2p(xsrc, ysrc, X(:), Y(:),...
         solution.q(:,1).*weights, solution.q(:,2).*weights, Lx, Ly,...
         'verbose', 1);
     
-    [pdlp_corrected,~] = mex_SQ_dlp_pressure(Xtar_sq(:)+1i*Ytar_sq(:), domain.z, domain.zp,...
+    % for special quadrature points must be inside reference cell
+    Xtar_sq = mod(X+Lx/2,Lx)-Lx/2;
+    Ytar_sq = mod(Y+Ly/2,Ly)-Ly/2;
+    
+    %correct using special quadrature
+    [pslp_corrected,~] = mex_SQ_slp_pressure(Xtar_sq(:)+1i*Ytar_sq(:), ...
+        domain.z, domain.zp, domain.quad_weights, domain.panel_breaks,...
+        domain.wazp, domain.z32, domain.zp32, domain.quad_weights32,...
+        domain.wazp32, solution.q(:,1)+1i*solution.q(:,2),...
+        pslp, domain.mean_panel_length, domain.extra.gridSolidmat, ...
+        domain.extra.Nrows, domain.extra.Ncols, domain.extra.panels2wall,...
+        domain.reference_cell,true);
+    
+    if isinf(solution.problem.eta)
+        p = pslp;
+        p_corrected = pslp_corrected;
+    else
+        
+        pdlp = StokesDLP_pressure_ewald_2p(xsrc, ysrc, X(:), Y(:), n1, n2,...
+            solution.q(:,1).*weights, solution.q(:,2).*weights, Lx, Ly,...
+            'verbose', 1);
+        
+        [pdlp_corrected,~] = mex_SQ_dlp_pressure(Xtar_sq(:)+1i*Ytar_sq(:),...
+            domain.z, domain.zp, domain.quad_weights, domain.panel_breaks,...
+            domain.wazp, domain.z32, domain.zp32, domain.quad_weights32,...
+            domain.wazp32, solution.q(:,1)+1i*solution.q(:,2), pdlp,...
+            domain.mean_panel_length, domain.extra.gridSolidmat, ...
+            domain.extra.Nrows, domain.extra.Ncols, domain.extra.panels2wall,...
+            domain.reference_cell, true);
+        
+        p = pdlp + solution.problem.eta*pslp;
+        p_corrected = pdlp_corrected + solution.problem.eta*pslp_corrected;
+    end
+    
+else % not periodic, only double-layer plus Stokeslets
+    
+    % note that the pressure is evaluated without an FMM, so it will be
+    % quite slow to evaluate if the number of target points is large
+    pdlp = evaluate_double_layer_pressure_direct(X, Y, domain.z, domain.zp,...
+        solution.q(:,1)+1i*solution.q(:,2), domain.quad_weights, true);
+    
+    pstokeslets = zeros(size(X(:)));
+    for i = 1:length(solution.forces)
+        R = (X(:) - domain.centers(i,1)) + 1i*(Y(:) - domain.centers(i,2));
+        pstokeslets = pstokeslets + real(conj(solution.forces(i))*R)./abs(R).^2;
+    end
+    
+    
+    [pdlp_corrected,~] = mex_SQ_dlp_pressure(X(:)+1i*Y(:), domain.z, domain.zp,...
         domain.quad_weights, domain.panel_breaks, domain.wazp, domain.z32,...
         domain.zp32, domain.quad_weights32, domain.wazp32, ...
         solution.q(:,1)+1i*solution.q(:,2),...
         pdlp,domain.mean_panel_length,domain.extra.gridSolidmat, ...
-        domain.extra.Nrows,domain.extra.Ncols,domain.extra.panels2wall,...
-        domain.reference_cell,true);
-
-    p = pdlp + solution.problem.eta*pslp;
-    p_corrected = pdlp_corrected + solution.problem.eta*pslp_corrected;
-end    
+        domain.extra.Nrows, domain.extra.Ncols, domain.extra.panels2wall,...
+        domain.reference_cell, false);
+    
+    p_corrected = pdlp_corrected + pstokeslets;
+    
+end
 
 % find points inside domain by applying stresslet identity
-[test1, test2] = StokesDLP_ewald_2p(xsrc, ysrc, X(:), Y(:), n1, n2,...
+if solution.problem.periodic
+    [test1, test2] = StokesDLP_ewald_2p(xsrc, ysrc, X(:), Y(:), n1, n2,...
         ones(length(n1),1).*weights, zeros(length(n1),1).*weights, Lx, Ly);
+else
+    % again, FMM for DLP assumes sources=targets, so we have to artifically
+    % add sources with strength zero
+    qtmp1 = [ones(length(xsrc),1).*weights; zeros(length(X(:)),1)];
+    qtmp2 = [zeros(length(xsrc),1).*weights; zeros(length(X(:)),1)];
+    ntmp1 = [n1(:); zeros(length(X(:)),1)];
+    ntmp2 = [n2(:); zeros(length(X(:)),1)];
+    
+    xtmp = [xsrc; X(:)];
+    ytmp = [ysrc; Y(:)];
+    
+    [test1, test2] = stokesDLPfmm(qtmp1, qtmp2, xtmp, ytmp, ntmp1, ntmp2);
+    
+    % extract data at target points only
+    test1 = -test1(length(xsrc)+1:end);
+    test2 = -test2(length(xsrc)+1:end);
+end
 
-    % correct using special quadrature
+
+% correct using special quadrature
 [test,~] = mex_SQ_dlp(X(:)+1i*Y(:), domain.z, domain.zp, domain.quad_weights, ...
-                domain.panel_breaks, domain.wazp, domain.z32, domain.zp32,...
-                domain.quad_weights32, domain.wazp32,ones(length(n1),1) + 1e-14*1i,...
-                test1 + 1i*test2,domain.mean_panel_length,domain.extra.gridSolidmat, ...
-                domain.extra.Nrows,domain.extra.Ncols,domain.extra.panels2wall,...
-                domain.reference_cell,true);
+    domain.panel_breaks, domain.wazp, domain.z32, domain.zp32,...
+    domain.quad_weights32, domain.wazp32,ones(length(n1),1) + 1e-14*1i,...
+    test1 + 1i*test2,domain.mean_panel_length,domain.extra.gridSolidmat, ...
+    domain.extra.Nrows,domain.extra.Ncols,domain.extra.panels2wall,...
+    domain.reference_cell,solution.problem.periodic);
 
 % anything that is greater than 0 is outside the fluid domain
 outside = find(solution.problem.stresslet_id_test(real(test)) == 1);
@@ -96,3 +148,11 @@ Y(outside) = nan;
 
 Pc = reshape(p_corrected, size(X));
 P = reshape(p, size(X));
+
+% add on driving pressure
+if solution.problem.periodic
+    P = P + solution.problem.pressure_gradient_x*X;
+    P = P + solution.problem.pressure_gradient_y*Y;
+    Pc = Pc + solution.problem.pressure_gradient_x*X;
+    Pc = Pc + solution.problem.pressure_gradient_y*Y;
+end
