9dc68acff38e70f7e3290b084e18050b0b2c604a
Lukas Bystricky <lukasby@kth.se>
2020-10-25 17:33:45 +0100

    code to compute on-surface pressure gradient

---
+++ src/matlab/special_quadrature/on_surface_evaluation/pressure_gradient_dlp_on_surface_correction.m
@@ -0,0 +1,49 @@
+function dPc = pressure_gradient_dlp_on_surface_correction(dP, solution)
+%PRESSURE_GRADIENT_DLP_ON_SURFACE_CORRECTION corrects the double-layer 
+%pressure gradient for on-surface evaluation. Adds on the jump as the 
+%target approaches the boundary from the fluid part of the domain.
+%
+%inputs:
+%-dP: uncorrected single-layer pressure evaluated at quadrature points on 
+%surface
+%-solution: solution structure containing geometry information, as well as
+%the density 
+%
+%output:
+%-dPc: corrected pressure gradient
+
+domain = solution.problem.domain;
+
+zsrc = domain.z;
+zpsrc = domain.zp;
+wsrc = domain.quad_weights;
+
+qsrc = (solution.q(:,1) + 1i*solution.q(:,2));
+
+dPc = dP;
+nw = size(domain.wall_indices,1);
+
+wall_start = 1;
+for i = 1:nw
+    
+    indices = domain.wall_indices(i,1):domain.wall_indices(i,2);
+    npan = length(indices)/16;
+    
+    panel_breaks_z = domain.panel_breaks(wall_start:wall_start + npan - 1);
+    
+    %subract off self contribution
+    for j = indices
+        
+        indices_tmp = indices;
+        indices_tmp(indices==j) = [];
+        
+        dPc(j) = dPc(j) - 2*1i*conj(sum(qsrc(indices_tmp).*wsrc(indices_tmp).*...
+                zpsrc(indices_tmp)./(zsrc(j) - zsrc(indices_tmp)).^3))/pi;
+    end    
+    
+    %add on special quadrature
+    dPc(indices) = dPc(indices) + 2*1i*conj(supersingular_on_surface_evaluation(qsrc(indices), ...
+                zsrc(indices), zpsrc(indices), wsrc(indices), panel_breaks_z))/pi; 
+            
+   wall_start = wall_start + npan + 1;       
+end
---
+++ src/matlab/special_quadrature/on_surface_evaluation/pressure_gradient_slp_on_surface_correction.m
@@ -0,0 +1,50 @@
+function dPc = pressure_gradient_slp_on_surface_correction(dP, solution)
+%PRESSURE_GRADIENT_SLP_ON_SURFACE_CORRECTION corrects the single-layer 
+%pressure gradient for on-surface evaluation. Adds on the jump as the 
+%target approaches the boundary from the fluid part of the domain.
+%
+%inputs:
+%-dP: uncorrected single-layer pressure evaluated at quadrature points on 
+%surface
+%-solution: solution structure containing geometry information, as well as
+%the density 
+%
+%output:
+%-dPc: corrected pressure
+
+domain = solution.problem.domain;
+
+zsrc = domain.z;
+zpsrc = domain.zp;
+nsrc = -1i*zpsrc./abs(zpsrc);
+wsrc = domain.quad_weights;
+
+qsrc = (solution.q(:,1) + 1i*solution.q(:,2));
+
+dPc = dP;
+nw = size(domain.wall_indices,1);
+
+wall_start = 1;
+for i = 1:nw
+    
+    indices = domain.wall_indices(i,1):domain.wall_indices(i,2);
+    npan = length(indices)/16;
+    
+    panel_breaks_z = domain.panel_breaks(wall_start:wall_start + npan - 1);
+    
+    %subract off self contribution
+    for j = indices
+        
+        indices_tmp = indices;
+        indices_tmp(indices==j) = [];
+        
+        dPc(j) = dPc(j) + 1i*conj(sum(qsrc(indices_tmp).*wsrc(indices_tmp).*...
+                zpsrc(indices_tmp)./(nsrc(indices_tmp).*(zsrc(j) - zsrc(indices_tmp)).^2)))/(2*pi);
+    end    
+    
+    %add on special quadrature
+    dPc(indices) = dPc(indices) - 1i*conj(hypersingular_on_surface_evaluation(qsrc(indices)./nsrc(indices), ...
+                zsrc(indices), zpsrc(indices), wsrc(indices), panel_breaks_z))/(2*pi); 
+            
+   wall_start = wall_start + npan + 1;       
+end
