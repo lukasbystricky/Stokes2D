50ed031d332182d10fc9a7fe4787c85da83c9a72
Lukas Bystricky <lukasby@kth.se>
2020-12-22 15:53:34 +0100

    moved data to a structure

--- src/setups/circles_periodic.m
+++ src/setups/circles_periodic.m
@@ -6,8 +6,8 @@ problem.Ly = input_params.box_size(2);
 
 problem.plot_domain = input_params.plot_domain;
 problem.periodic = 1;
-problem.pressure_gradient_x = input_params.pressure_drop_x / problem.Lx;
-problem.pressure_gradient_y = input_params.pressure_drop_y / problem.Ly;
+problem.pressure_gradient_x = -input_params.pressure_drop_x / problem.Lx;
+problem.pressure_gradient_y = -input_params.pressure_drop_y / problem.Ly;
 problem.name = input_params.name;
 problem.gmres_tol = input_params.gmres_tol;
 problem.eta = input_params.eta;
@@ -17,24 +17,23 @@ problem.stresslet_id_test = input_params.stresslet_id_test;
 radii = input_params.radii;
 centers = input_params.centers;
 
-walls = cell(length(radii));
+problem.walls = cell(length(radii),1);
 
 for i = 1:length(radii)
-   walls{i} =  @(T) geometry_circle(radii(i),centers(i),T);
+   problem.walls{i} =  @(T) geometry_circle(radii(i),centers(i),T);
 end
 
 if length(input_params.panels) > 1
     problem.panels = input_params.panels;
 else
-    problem.panels = input_params.panels*ones(length(walls),1);
+    problem.panels = input_params.panels*ones(length(problem.walls),1);
 end
 
-problem.domain = discretize_domain(walls, problem.panels,...
+problem.domain = discretize_domain(problem.walls, problem.panels,...
             problem.Lx, problem.Ly);
 
 % no-slip boundary conditions
 problem.boundary_conditions = @(z) 0*imag(z);
-
 
 if problem.plot_domain
     plot_domain(problem);
