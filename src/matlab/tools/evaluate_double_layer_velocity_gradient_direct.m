52e3ba9c356863adbe8de30c6380c2e6c6f7bfec
David Krantz <brante_krantz@hotmail.com>
2021-09-24 17:07:40 +0200

    evaluates the velocity gradient of the single-layer, double-layer, Stokeslet, and rotlet

---
+++ src/matlab/tools/evaluate_double_layer_velocity_gradient_direct.m
@@ -0,0 +1,36 @@
+function dV = evaluate_double_layer_velocity_gradient_direct(xtar, ytar, z, btar, zp, q, w)
+ztar = xtar + 1i*ytar;
+n = -1i*zp./abs(zp);
+
+% inefficient, but will do for now
+Ih_q = evaluate_hypersingular_integral(xtar, ytar, z, zp, q, w);
+Ih_rqn = evaluate_hypersingular_integral(xtar, ytar, z, zp, real(q.*conj(n))./n, w);
+Is_q = evaluate_supersingular_integral(xtar, ytar, z, zp, q, w);
+Is_tq = evaluate_supersingular_integral(xtar, ytar, z, zp, conj(z).*q, w);
+
+dV = -1i/(2*pi) * (btar.*real(Ih_q) - conj(btar.*(Ih_rqn+conj(ztar).*Is_q-Is_tq)));
+end
+
+function  Ic = evaluate_cauchy_integral(xtar, ytar, z, zp, q, w)
+Ic = zeros(size(xtar));
+for k = 1:length(xtar)
+    rho = (xtar(k) + 1i*ytar(k)) - z;
+    Ic(k) = sum(q.*zp.*w./rho);
+end
+end
+
+function  Ih = evaluate_hypersingular_integral(xtar, ytar, z, zp, q, w)
+Ih = zeros(size(xtar));
+for k = 1:length(xtar)
+    rho = (xtar(k) + 1i*ytar(k)) - z;
+    Ih(k) = sum(q.*zp.*w./rho.^2);
+end
+end
+
+function  Is = evaluate_supersingular_integral(xtar, ytar, z, zp, q, w)
+Is = zeros(size(xtar));
+for k = 1:length(xtar)
+    rho = (xtar(k) + 1i*ytar(k)) - z;
+    Is(k) = sum(q.*zp.*w./rho.^3);
+end
+end
---
+++ src/matlab/tools/evaluate_single_layer_velocity_gradient_direct.m
@@ -0,0 +1,43 @@
+function dV = evaluate_single_layer_velocity_gradient_direct(xtar, ytar, z, btar, zp, q, w)
+ztar = xtar + 1i*ytar;
+n = -1i*zp./abs(zp);
+%dV = zeros(size(xtar));
+
+Ih_qn = evaluate_hypersingular_integral(xtar, ytar, z, zp, q./n, w);
+Ih_tqn = evaluate_hypersingular_integral(xtar, ytar, z, zp, conj(z).*q./n, w);
+Ic_cqn = evaluate_cauchy_integral(xtar, ytar, z, zp, conj(q)./n, w);
+Ic_qn = evaluate_cauchy_integral(xtar, ytar, z, zp, q./n, w);
+
+dV = -1i*conj(btar)/2 .* (ztar.*conj(Ih_qn) - conj(Ih_tqn) + conj(Ic_cqn)) + ...
+    1i*btar.*real(Ic_qn);
+dV = 1/(4*pi)*dV;
+end
+
+function  Ic = evaluate_cauchy_integral(xtar, ytar, z, zp, q, w)
+Ic = zeros(size(xtar));
+for k = 1:length(xtar)
+    rho = (xtar(k) + 1i*ytar(k)) - z;
+    Ic(k) = sum(q.*zp.*w./rho);
+end
+end
+
+function  Ih = evaluate_hypersingular_integral(xtar, ytar, z, zp, q, w)
+Ih = zeros(size(xtar));
+for k = 1:length(xtar)
+    rho = (xtar(k) + 1i*ytar(k)) - z;
+    Ih(k) = sum(q.*zp.*w./rho.^2);
+end
+end
+
+function  Is = evaluate_supersingular_integral(xtar, ytar, z, zp, q, w)
+Is = zeros(size(xtar));
+for k = 1:length(xtar)
+    rho = (xtar(k) + 1i*ytar(k)) - z;
+    Is(k) = sum(q.*zp.*w./rho.^3);
+end
+end
+
+%rho = (xtar(k) + 1i*ytar(k)) - z;
+%sum(conj(zsrc(j))*qtmp./(ntmp.*r.^2))
+%sum(conj(ztmp).*qtmp./(ntmp.*r.^2))
+%sum(conj(qtmp)./(ntmp.*r)).*wtmp.*zptmp))
