d9f9845bc493f00219e80d77314a71d9c0859171
David Krantz <brante_krantz@hotmail.com>
2022-01-03 11:11:59 +0100

    initial periodic stress computations

--- examples/demos/demo_periodic_pouseille_flow.m
+++ examples/demos/demo_periodic_pouseille_flow.m
@@ -16,8 +16,8 @@ input_params = default_input_params('pouseuille_demo', 1);
 % modify structure as needed
 input_params.box_size = [3,5];
 input_params.h = 0.5;    % pipe walls at +-0.5
-input_params.panels = 20;
-input_params.eta = 1;
+input_params.panels = 10;
+input_params.eta = inf;
 input_params.plot_domain = 0;
 
 problem = flat_pipe_periodic(input_params);
@@ -49,6 +49,16 @@ exact_solution_p_grad_avg = [(2*h*Lx*p)/volume; 0];
 [Uxc, Uyc, Vxc, Vyc, Ux, Uy, Vx, Vy] = evaluate_velocity_gradient(solution, X, Y);
 [Px, Py] = evaluate_pressure_gradient(solution, X, Y);
 [omegac, omega] = evaluate_vorticity(solution, X, Y);
+
+%%
+[sxxc, sxyc, syxc, syyc, sxx, sxy, syx, syy, ~, ~] = evaluate_stress(solution, X, Y);
+sigma = exact_solution_sigma(X,Y,p);
+
+%% Stresses from pressure and velocity gradient calculations (works)
+velsxxc = -Pc+2*Uxc;
+velsxyc = Vxc+Uyc;
+velsyxc = Uyc+Vxc;
+velsyyc = -Pc+2*Vyc;
 
 %% compute averages
 solution.trim = 0;      % do not remove values outside the domain
@@ -131,7 +141,99 @@ colorbar
 %caxis([-16,-1]);
 axis equal
 title('omega: log_{10}(relative error)');
+%%
+h2 = figure();
+subplot(2,2,1);
+hold on;
+contourf(X,Y,log10(abs(sxxc - sigma(:,:,1))./...
+    max(max(abs(sigma(:,:,1)))) + eps));
+colorbar;
+axis equal;
+title('sxx: log_{10} relative error');
 
+subplot(2,2,2);
+hold on;
+contourf(X,Y,log10(abs(sxyc - sigma(:,:,3))./...
+    max(max(abs(sigma(:,:,3)))) + eps));
+colorbar;
+axis equal;
+title('sxy: log_{10} relative error');
+
+subplot(2,2,3);
+hold on;
+contourf(X,Y,log10(abs(syxc - sigma(:,:,2))./...
+    max(max(abs(sigma(:,:,2)))) + eps));
+colorbar;
+axis equal;
+title('syx: log_{10} relative error');
+
+subplot(2,2,4);
+hold on;
+contourf(X,Y,log10(abs(syyc - sigma(:,:,4))./...
+    max(max(abs(sigma(:,:,4)))) + eps));
+colorbar;
+axis equal;
+title('syy: log_{10} relative error');
+%%
+h3 = figure();
+subplot(2,2,1);
+hold on;
+contourf(X,Y,sigma(:,:,1));
+colorbar;
+axis equal;
+title('sxx true');
+
+subplot(2,2,2);
+hold on;
+contourf(X,Y,sigma(:,:,2));
+colorbar;
+axis equal;
+title('sxy true');
+
+subplot(2,2,3);
+hold on;
+contourf(X,Y,sigma(:,:,3));
+colorbar;
+axis equal;
+title('syx true');
+
+subplot(2,2,4);
+hold on;
+contourf(X,Y,sigma(:,:,4));
+colorbar;
+axis equal;
+title('syy true');
+
+%%
+h4 = figure();
+subplot(2,2,1);
+hold on;
+contourf(X,Y,sxxc);
+colorbar;
+axis equal;
+title('sxx num');
+
+subplot(2,2,2);
+hold on;
+contourf(X,Y,sxyc);
+colorbar;
+axis equal;
+title('sxy num');
+
+subplot(2,2,3);
+hold on;
+contourf(X,Y,syxc);
+colorbar;
+axis equal;
+title('syx num');
+
+subplot(2,2,4);
+hold on;
+contourf(X,Y,syyc);
+colorbar;
+axis equal;
+title('syy num');
+    
 %% individual figures
 set(groot,'defaultAxesTickLabelInterpreter','latex');
 
@@ -177,3 +279,19 @@ xlabel({'$x$'},'interpreter','latex','fontsize',16);
 ylabel({'$y$'},'interpreter','latex','fontsize',16);
 title('Absolute error $\nabla p$ ($\log_{10}$)','interpreter','latex','fontsize',16);
 %saveas(gca,'/afs/kth.se/home/d/a/davkra/Documents/phd_project_presentation_2021/abs_err_nabla_p','epsc');
+
+function S = exact_solution_sigma(X,Y,p)
+[m,n] = size(X);
+S = zeros(m,n,4);
+for i = 1:m
+    for j = 1:n
+        x = X(i,j);
+        y = Y(i,j);
+
+        s = [-p*x p*y;
+             p*y -p*x];
+
+        S(i,j,:) = s(:);
+    end
+end
+end
